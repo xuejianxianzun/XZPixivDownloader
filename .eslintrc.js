15e53af9b2231d641b8c98a414730055d0d252e6
xuejianxianzun <1247756698@qq.com>
2019-01-28 14:19:25 +0800

    eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": false,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 8
+    },
+    "rules": {
+        "indent": ["error", "tab"],
+        "linebreak-style": ["error", "windows"],
+        "quotes": ["error", "single"],
+        "semi": [2, "always"],
+        "no-console": "off"
+    },
+    "globals": {
+        "GM_xmlhttpRequest": true,
+        "GM_info": true,
+        "GM_download": true,
+    }
+};
